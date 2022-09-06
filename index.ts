fdbd97d6f930adc8581ba38275fcc58178d12961
Jessy <c.j91350@gmail.com>
2022-09-06 12:56:18 +0200

    Refacto, and enable cors

--- index.ts
+++ index.ts
@@ -1,25 +1,38 @@
+import dotenv from "dotenv";
+import { createServer } from 'http';
 import { Server } from "socket.io";
+import express, { Response } from "express";
 import { ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData } from "./types";
-const { createServer } = require("http");
-const express = require("express");
+
+dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {});
+
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+app.get("/", (_, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
 
 // Server to client
 io.on("connection", (socket) => {
   console.log(socket.id);
-  
   // works when broadcasting to a room
   //io.to("room1").emit("basicEmit", 1, "2", Buffer.from([3]));
 });
 
-// Client to server
-io.on("connection", (socket) => {
-  socket.on("hello", () => {
-    console.log("hey")
-  });
-});
+// // Client to server
+// io.on("connection", (socket) => {
+//   socket.on("hello", () => {
+//     console.log("hey")
+//   });
+// });
 
-httpServer.listen(3000);
+httpServer.listen(8000, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
